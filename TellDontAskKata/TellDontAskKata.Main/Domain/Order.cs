4ff1c46c9a69ff4be7647cc980a47ab6c36f9ac5
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-11-20 19:14:30 +0100

    move order item creation

--- TellDontAskKata/TellDontAskKata.Main/Domain/Order.cs
+++ TellDontAskKata/TellDontAskKata.Main/Domain/Order.cs
@@ -1,52 +1,81 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using TellDontAskKata.Main.UseCase;
 
-namespace TellDontAskKata.Main.Domain
+namespace TellDontAskKata.Main.Domain;
+
+public class Order
 {
-    public class Order
+    public decimal Total { get; set; }
+    public string Currency { get; set; }
+    public IList<OrderItem> Items { get; set; }
+    public decimal Tax { get; set; }
+    public OrderStatus Status { get; set; }
+    public int Id { get; set; }
+
+    public void Ship()
     {
-        public decimal Total { get; set; }
-        public string Currency { get; set; }
-        public IList<OrderItem> Items { get; set; }
-        public decimal Tax { get; set; }
-        public OrderStatus Status { get; set; }
-        public int Id { get; set; }
-
-        public void Ship()
+        if (Status is OrderStatus.Created or OrderStatus.Rejected)
         {
-            if (Status is OrderStatus.Created or OrderStatus.Rejected)
-            {
-                throw new OrderCannotBeShippedException();
-            }
-
-            if (Status is OrderStatus.Shipped)
-            {
-                throw new OrderCannotBeShippedTwiceException();
-            }
-
-            Status = OrderStatus.Shipped;
+            throw new OrderCannotBeShippedException();
         }
 
-        public void ChangeApprove(bool approvedState)
+        if (Status is OrderStatus.Shipped)
         {
-            if (Status is OrderStatus.Shipped)
-            {
-                throw new ShippedOrdersCannotBeChangedException();
-            }
-
-            if (approvedState && Status is OrderStatus.Rejected)
-            {
-                throw new RejectedOrderCannotBeApprovedException();
-            }
-
-            if (!approvedState && Status is OrderStatus.Approved)
-            {
-                throw new ApprovedOrderCannotBeRejectedException();
-            }
-
-            Status = approvedState
-                ? OrderStatus.Approved
-                : OrderStatus.Rejected;
+            throw new OrderCannotBeShippedTwiceException();
         }
+
+        Status = OrderStatus.Shipped;
+    }
+
+    public void ChangeApprove(bool approvedState)
+    {
+        if (Status is OrderStatus.Shipped)
+        {
+            throw new ShippedOrdersCannotBeChangedException();
+        }
+
+        if (approvedState && Status is OrderStatus.Rejected)
+        {
+            throw new RejectedOrderCannotBeApprovedException();
+        }
+
+        if (!approvedState && Status is OrderStatus.Approved)
+        {
+            throw new ApprovedOrderCannotBeRejectedException();
+        }
+
+        Status = approvedState
+            ? OrderStatus.Approved
+            : OrderStatus.Rejected;
+    }
+
+    public void CreateItemFromProduct(Product? product, int quantity)
+    {
+        if (product is null)
+        {
+            throw new UnknownProductException();
+        }
+
+        var unitaryTax = Round(product.Price / 100m * product.Category.TaxPercentage);
+        var unitaryTaxedAmount = Round(product.Price + unitaryTax);
+        var taxedAmount = Round(unitaryTaxedAmount * quantity);
+        var taxAmount = Round(unitaryTax * quantity);
+
+        var orderItem = new OrderItem
+        {
+            Product = product,
+            Quantity = quantity,
+            Tax = taxAmount,
+            TaxedAmount = taxedAmount
+        };
+        Items.Add(orderItem);
+        Total += taxedAmount;
+        Tax += taxAmount;
+    }
+
+    private static decimal Round(decimal amount)
+    {
+        return decimal.Round(amount, 2, MidpointRounding.ToPositiveInfinity);
     }
 }
--- TellDontAskKata/TellDontAskKata.Main/UseCase/OrderCreationUseCase.cs
+++ TellDontAskKata/TellDontAskKata.Main/UseCase/OrderCreationUseCase.cs
@@ -28,39 +28,14 @@ namespace TellDontAskKata.Main.UseCase
                 Tax = 0m
             };
 
-            foreach(var itemRequest in request.Requests){
+            foreach (var itemRequest in request.Requests)
+            {
                 var product = _productCatalog.GetByName(itemRequest.ProductName);
 
-                if (product == null)
-                {
-                    throw new UnknownProductException();
-                }
-                else
-                {
-                    var unitaryTax = Round((product.Price / 100m) * product.Category.TaxPercentage);
-                    var unitaryTaxedAmount = Round(product.Price + unitaryTax);
-                    var taxedAmount = Round(unitaryTaxedAmount * itemRequest.Quantity);
-                    var taxAmount = Round(unitaryTax * itemRequest.Quantity);
-
-                    var orderItem = new OrderItem
-                    {
-                        Product = product,
-                        Quantity = itemRequest.Quantity,
-                        Tax = taxAmount,
-                        TaxedAmount = taxedAmount
-                    };
-                    order.Items.Add(orderItem);
-                    order.Total += taxedAmount;
-                    order.Tax += taxAmount;
-                }
+                order.CreateItemFromProduct(product, itemRequest.Quantity);
             }
 
             _orderRepository.Save(order);
-        }
-
-        private static decimal Round(decimal amount)
-        {
-            return decimal.Round(amount, 2, System.MidpointRounding.ToPositiveInfinity);
         }
     }
 }
